fe1a15b4676cf558afcb25dc671c63ee64dd8291
JoaquinMeza1326 <u20211c113@upc.edu.pe>
2025-05-11 16:08:27 -0500

    EventType updated implements.java

--- src/main/java/backend/project/entities/EventType.java
+++ src/main/java/backend/project/entities/EventType.java
@@ -6,9 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-//Cultural event type
-
+//updated
 import java.util.List;
 
 @Entity
