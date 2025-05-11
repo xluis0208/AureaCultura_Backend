c04af4c721d4f822c605a1f66c24cda6cbeee94e
JoaquinMeza1326 <u20211c113@upc.edu.pe>
2025-05-11 18:09:26 -0500

    Update KeyRepeatedDataException.java

--- src/main/java/backend/project/exceptions/KeyRepeatedDataException.java
+++ src/main/java/backend/project/exceptions/KeyRepeatedDataException.java
@@ -1,6 +1,5 @@
 package backend.project.exceptions;
  
-
 public class KeyRepeatedDataException extends RuntimeException {
     public KeyRepeatedDataException(String message) {
         super(message);
