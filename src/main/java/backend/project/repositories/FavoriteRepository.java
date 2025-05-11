78ea9c69127db35ddcba8ac730e0ce8a3b7da16e
Luis Alberto Gonzales Espinoza <u20231d650@upc.edu.pe>
2025-05-11 18:32:02 -0500

    Update FavoriteRepository.java

--- src/main/java/backend/project/repositories/FavoriteRepository.java
+++ src/main/java/backend/project/repositories/FavoriteRepository.java
@@ -1,5 +1,5 @@
 package backend.project.repositories;
-
+// cambios en este documento
 import backend.project.entities.Favorite;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
